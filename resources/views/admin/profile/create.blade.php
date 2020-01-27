64d2b036b22bbd9b4a256dc1dfb85ccf4c0514a5
EC2 Default User <ec2-user@ip-172-31-40-86.us-east-2.compute.internal>
2020-01-27 04:19:39 +0000

    php_laravel_12

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>MyNews</title>
+    <title>@yield('title')</title>
   </head>
   <body>
-    <h1>Myニュース追加画面</h1>
+    
   </body>
 </html>
+
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- admin.blade.phpの@yield('title')に'プロフィール'を埋め込む --}}
+@section('title', 'プロフィール')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto">
+        <h2>Myプロフィール</h2>
+      </div>
+    </div>
+  </div>
+@endsection
